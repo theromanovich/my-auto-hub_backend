4eaeedf89793e59223d65bad487b653160dcb76e
yurii.k <yurii.k@maxitechs.com>
2022-12-19 17:29:58 +0100

    added cloudinary

---
+++ strapi-app/config/plugins.ts
@@ -0,0 +1,16 @@
+module.exports = ({ env }) => {
+  console.log('>>>>CLOUDINARY_NAME:', env("CLOUDINARY_NAME"));
+
+  return {
+    upload: {
+      config: {
+        provider: "cloudinary",
+        providerOptions: {
+          cloud_name: env("CLOUDINARY_NAME"),
+          api_key: env("CLOUDINARY_KEY"),
+          api_secret: env("CLOUDINARY_SECRET"),
+        },
+      },
+    },
+  };
+}
